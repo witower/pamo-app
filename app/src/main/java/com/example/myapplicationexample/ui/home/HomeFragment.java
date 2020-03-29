c10b8ecb72cca2730c84f864b8b27d1a84aea896
witower <25256638+witower@users.noreply.github.com>
2020-03-29 16:53:45 +0200

    Initial top destination fragments

--- app/src/main/java/com/example/myapplicationexample/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplicationexample/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,16 +15,21 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplicationexample.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private EditText heightEditText;
+    private EditText weightEditText;
+    private EditText ageEditText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +37,41 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        heightEditText = root.findViewById(R.id.input_height);
+        weightEditText = root.findViewById(R.id.input_weight);
+        ageEditText = root.findViewById(R.id.input_age);
+
         return root;
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.button_show_recipes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snacky = Snackbar.make(v, R.string.button_show_recipes, Snackbar.LENGTH_LONG);
+                snacky.show();
+            }
+        });
+
+        view.findViewById(R.id.button_go_to_form).setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countMe(v);
+            }
+        }));
+    }
+
+    private void countMe(View view) {
+        Integer height = Integer.parseInt(heightEditText.getText().toString());
+        Integer weight = Integer.parseInt(weightEditText.getText().toString());
+        Integer age = Integer.parseInt(ageEditText.getText().toString());
+        //todo validation
+//        count++;
+//
+//        showCountTextView.setText(count.toString());
+//        PPM (kobiety) = SWE (spoczynkowy wydatek energetyczny kcal) = 655,1 + (9,563 x masa ciała [kg]) + (1,85 x wzrost [cm]) – (4,676 x [wiek])
+//        PPM (mężczyźni) = SWE (spoczynkowy wydatek energetyczny kcal) = 66,5 + (13,75 x masa ciała [kg]) + (5,003 x wzrost [cm]) – (6,775 x [wiek])
+    }
+
 }
