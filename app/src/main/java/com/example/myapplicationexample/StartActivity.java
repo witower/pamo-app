55d7e805ae62d2a2211173f4a4182048bd561be7
witower <25256638+witower@users.noreply.github.com>
2020-04-05 00:20:21 +0200

    Start splash screen

---
+++ app/src/main/java/com/example/myapplicationexample/StartActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplicationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainActivity = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(mainActivity);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
