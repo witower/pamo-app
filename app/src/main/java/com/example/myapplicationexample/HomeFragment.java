c6a6e58bbbcbc27914f142d4dad2a707d17ff864
witower <25256638+witower@users.noreply.github.com>
2020-04-05 11:29:18 +0200

    BMI, calories, recipe with bottom navigation for next tasks

--- app/src/main/java/com/example/myapplicationexample/HomeFragment.java
+++ app/src/main/java/com/example/myapplicationexample/HomeFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,7 +18,34 @@ import androidx.navigation.Navigation;
 import com.example.myapplicationexample.R;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.text.DecimalFormat;
+
 public class HomeFragment extends Fragment {
+
+    private static DecimalFormat decimalFormatTwoPoints = new DecimalFormat("#.##");
+
+    private int height;
+    private int weight;
+    private int age;
+    private double bmi;
+
+    private boolean isMale;
+    private boolean isFemale;
+
+    private EditText heightInput;
+    private EditText weightInput;
+    private EditText ageInput;
+    private RadioGroup genderRadio;
+    private RadioButton femaleRadio;
+    private RadioButton maleRadio;
+    private TextView resultBmiTextView;
+    private TextView resultCaloriesTextView;
+    private TextView resultRecipeTextView;
+
+    private TextView testTextView;
+
+    private Snackbar snacky;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -22,21 +53,104 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        resultBmiTextView = root.findViewById(R.id.result_bmi);
+        resultCaloriesTextView = root.findViewById(R.id.result_calories);
+        resultRecipeTextView = root.findViewById(R.id.result_recipe);
+
+        heightInput = root.findViewById(R.id.input_height);
+        weightInput = root.findViewById(R.id.input_weight);
+        ageInput = root.findViewById(R.id.input_age);
+        genderRadio = root.findViewById(R.id.radio_group_gender);
+        femaleRadio = root.findViewById(R.id.radio_button_female);
+        maleRadio = root.findViewById(R.id.radio_button_male);
+
+        Button doCalcButton = root.findViewById(R.id.button_do_calc);
+        doCalcButton.setOnClickListener(onDoCalcButtonClickListener);
+
+
     return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        view.findViewById(R.id.button_show_recipes).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_recipeFragment));
-//        view.findViewById(R.id.button_go_to_form).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_formFragment));
+        snacky = Snackbar.make(view, R.string.app_name, Snackbar.LENGTH_LONG);
+    }
+
+    private final View.OnClickListener onDoCalcButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            calculate();
+        }
+    };
+
+    private int safeAlikeParse(String s) {
+        int r;
+        try {
+            r = Integer.parseInt(s);
+        }
+        catch (NumberFormatException e) {
+            r = 0;
+        }
+        return r;
+    }
+
+    private boolean getFormData() {
+        height = safeAlikeParse(heightInput.getText().toString());
+        weight = safeAlikeParse(weightInput.getText().toString());
+        age = safeAlikeParse(ageInput.getText().toString());
+        isFemale = femaleRadio.isChecked();
+        isMale = maleRadio.isChecked();
+
+        return !(height == 0 | weight == 0 | age == 0 | !(isFemale | isMale));
+    }
+
+    private void clearResults(){
+        testTextView.setText("");
+        resultBmiTextView.setText("");
+        resultCaloriesTextView.setText("");
+        resultRecipeTextView.setText("");
+    }
+
+    private void calculate() {
+        calcBmi();
+        if (getFormData()){
+            calcBmi();
+            calcCalories();
+            showRecipe();
+        }
+        else {
+            snacky.setText(R.string.snack_wrong_data).show();
+            clearResults();
+        }
 
     }
 
+    private void calcBmi() {
+        double heightMeters = (double)height/100;
+        bmi = weight / (heightMeters * heightMeters);
+        resultBmiTextView.setText(decimalFormatTwoPoints.format(bmi));
+    }
 
+    private void calcCalories() {
+        double calories;
 
-    private void snackExample() {
-        Snackbar snacky = Snackbar.make(getView(), R.string.button_show_recipes, Snackbar.LENGTH_LONG);
-        snacky.show();
+        if (isFemale)
+            calories = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+        else if (isMale)
+            calories = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age);
+        else calories = 0;
+
+        resultCaloriesTextView.setText(decimalFormatTwoPoints.format(calories));
+    }
+
+    private void showRecipe() {
+        if (bmi > 25) {
+            resultRecipeTextView.setText(R.string.recipe_salad);
+        }
+        else {
+            resultRecipeTextView.setText(R.string.recipe_chips);
+        }
+
     }
 
 }
